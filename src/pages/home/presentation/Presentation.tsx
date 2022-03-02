d77c0eabc0659cd1490ae4ff7365369271536d08
Nicolas Carrasco <nicolas.carrasco@epitech.eu>
2022-03-02 20:00:10 +0100

    Refactor pres part

--- src/pages/home/presentation/Presentation.tsx
+++ src/pages/home/presentation/Presentation.tsx
@@ -2,27 +2,33 @@ import React from "react";
 import styled from "styled-components";
 
 import { ReactComponent as Blob } from "../../../assets/blob-pres.svg";
+import photo from "../../../assets/NicolasCarrasco.png";
+import cv from "../../../assets/CV.pdf";
 
 const Presentation = () => {
   return (
     <Container>
-      <PhotoWrapper>
-        <PhotoBase>
+      <InfoWrapper>
+        <PhotoItemWrapper>
+          <PhotoWrapper>
+            <Photo src={photo} />
+          </PhotoWrapper>
           <BubbleWrapper>
             <Bubble width={20} marginLeft={6} animationDelay={0} />
             <Bubble width={15} marginLeft={32} animationDelay={0.5} />
-            <Bubble width={24} marginLeft={0} animationDelay={1} />
+            <Bubble width={24} marginLeft={0} animationDelay={1.3} />
           </BubbleWrapper>
-        </PhotoBase>
-      </PhotoWrapper>
-      <PresWrapper>
-        <Blob />
-        <PresText>
-          Développeur front-end en 4ème année de l’école d’informatique Epitech
-        </PresText>
-      </PresWrapper>
+        </PhotoItemWrapper>
+        <PresWrapper>
+          <Blob />
+          <PresText>
+            Développeur front-end en 4ème année de l’école d’informatique
+            Epitech
+          </PresText>
+        </PresWrapper>
+      </InfoWrapper>
       <CvWrapper>
-        <CvFish>
+        <CvFish href={cv} target="_blank">
           <CvText>Mon Cv</CvText>
         </CvFish>
       </CvWrapper>
@@ -38,29 +44,60 @@ const Container = styled.div`
   padding: 0 13vw;
 `;
 
-const PhotoWrapper = styled.div`
+const InfoWrapper = styled.div`
   display: flex;
-  margin-bottom: 40px;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  margin-bottom: 80px;
+
+  @media only screen and (max-device-width: 950px) {
+    flex-direction: column;
+  }
 `;
 
-const PhotoBase = styled.div`
+const PhotoItemWrapper = styled.div`
+  display: flex;
+  margin-right: 170px;
   width: 200px;
   height: 200px;
-  border-radius: 50%;
-  background: linear-gradient(
-    90deg,
-    rgba(4, 129, 177, 1) 0%,
-    rgba(0, 181, 217, 1) 62%,
-    rgba(59, 234, 245, 1) 100%
-  );
   position: relative;
+
+  @media only screen and (max-device-width: 950px) {
+    margin-right: 0;
+    margin-bottom: 80px;
+  }
+`;
+
+const PhotoWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background: radial-gradient(circle closest-side, #0000, #0000),
+    linear-gradient(
+      90deg,
+      rgba(4, 129, 177, 1) 0%,
+      rgba(0, 181, 217, 1) 62%,
+      rgba(59, 234, 245, 1) 100%
+    );
+  border-radius: 0 0 50% 50%;
+  overflow: hidden;
+  border-radius: 50%;
+  position: relative;
+`;
+
+const Photo = styled.img`
+  position: absolute;
+  width: 210px;
+  bottom: 0;
 `;
 
 const BubbleWrapper = styled.div`
   display: flex;
   flex-direction: column;
   position: absolute;
-  right: -60px;
+  top: -10px;
+  right: -50px;
+  height: 100%;
 `;
 
 const Bubble = styled.div<{
@@ -73,7 +110,7 @@ const Bubble = styled.div<{
   margin-left: ${({ marginLeft }) => `${marginLeft}px`};
   border-radius: 50%;
   border: ${({ theme }) => `${theme.colors.textPrimary} 1px solid`};
-  animation: bounceBubble 3s infinite;
+  animation: bounceBubble 3.5s infinite;
   animation-delay: ${({ animationDelay }) => `${animationDelay}s`};
 
   @keyframes bounceBubble {
@@ -83,10 +120,10 @@ const Bubble = styled.div<{
       transform: translateY(0);
     }
     25% {
-      transform: translateY(-2px);
+      transform: translateY(-5px);
     }
     75% {
-      transform: translateY(-2px);
+      transform: translateY(-5px);
     }
   }
 `;
@@ -95,7 +132,6 @@ const PresWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
   position: relative;
-  margin-bottom: 40px;
 `;
 
 const PresText = styled.p`
@@ -117,6 +153,7 @@ const PresText = styled.p`
 
 const CvWrapper = styled.div`
   display: flex;
+  justify-content: center;
   margin-left: 57px;
 `;
 
@@ -131,6 +168,7 @@ const CvFish = styled.a`
   align-items: center;
   justify-content: center;
   animation: bounce 2s infinite;
+  text-decoration: none;
 
   background-color: ${({ theme }) => theme.colors.coral};
 
