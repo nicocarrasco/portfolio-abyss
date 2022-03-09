6061f482c7154da1fa014f105094878f38b37c90
Nicolas Carrasco <nicolas.carrasco@epitech.eu>
2022-03-09 12:31:07 +0100

    Correcting description

--- src/pages/home/projects/Projects.tsx
+++ src/pages/home/projects/Projects.tsx
@@ -8,6 +8,10 @@ type Project = {
   title: string;
   date: string;
   description: string;
+  link?: {
+    name: string;
+    url: string;
+  };
 };
 
 const projects: Project[] = [
@@ -15,7 +19,7 @@ const projects: Project[] = [
     title: "Machine learning (apprentissage)",
     date: "Janvier 2022 - Mars 2022",
     description:
-      "Apprentissage et création d'algorithmes de ML en Python uniquement avec les libraires standards ainsi que pandas et numpy.\n\nLes algorithmes que j'ai reproduit sont : Naives Bayes Classifier, Decision Tree Classifier et un Neural Network. Ces algorithmes ont été réalisé en tant que projet d'apprentissage lors de ma 4ème année à Epitech.",
+      "Apprentissage et création d'algorithmes de ML en Python uniquement avec les librairies standards ainsi que pandas et numpy.\n\nLes algorithmes que j'ai reproduits sont : Naives Bayes Classifier, Decision Tree Classifier et un Neural Network. Ces algorithmes ont été réalisé en tant que projet d'apprentissage lors de ma 4ème année à Epitech.",
   },
 
   {
@@ -23,13 +27,14 @@ const projects: Project[] = [
     date: "Novembre 2020 - Juillet 2021",
     description:
       "Stage de 5 mois en alternance puis de 3 mois en temps plein réalisé chez Imparato, application visant à aider les comédiens à répéter leurs textes.\n\nOutre la correction de bug et l'amélioration de l'UI/UX, j'ai participé à la création de nouvelles fonctionnalités sur l'app web et mobile comme par exemple l'ajout de notes et de marque-pages sur les textes ou encore l'implémentation des bruitages sur l'application.\n\nLes technos utilisées étaient React Js et React Native.",
+    link: { name: "Imparato", url: "https://www.imparato.io/" },
   },
 
   {
     title: "Area (apprentissage)",
     date: "Janvier 2021 - Mars 2021",
     description:
-      "Projet réalisé à 6 étudiants pour reproduire le fonctionnement d'un site comme Zapier: permettre à un utilisateur de connecter différents services afin d'automatiser des actions entre elles (par exemple, lorsque l'utilisateur crée une nouvelle PR sur Github, cela crée une nouvelle tâche au nom de la PR sur son Trello).\n\n J'ai travaillé sur l'interface web ainsi que sur le serveur (authentification et webhooks).\n\nLes technos que j'ai utilisé étaient React Js et Node Js.",
+      "Projet réalisé à 6 étudiants pour reproduire le fonctionnement d'un site comme Zapier: permettre à un utilisateur de connecter différents services afin d'automatiser des actions entre elles (par exemple, lorsque l'utilisateur crée une nouvelle PR sur Github, cela crée une nouvelle tâche au nom de la PR sur son Trello).\n\n J'ai travaillé sur l'interface web ainsi que sur le serveur (authentification et webhooks).\n\nLes technos que j'ai utilisées étaient React Js et Node Js.",
   },
 
   {
@@ -44,13 +49,15 @@ const projects: Project[] = [
     date: "Septembre 2019 - Décembre 2019",
     description:
       "Stage à temps plein réalisé chez Sportbak, application permettant aux joueurs de footsalle de récolter et visualiser leurs stats individuelles et collectives sur un match.\n\nAu cours de mon stage chez Sportbak j'ai travaillé sur la création de ligues et des tournois de footsalle sur l'application ainsi que la visualisation des statistiques des joueurs lors de ces ligues et tournois.\n\nLes technos utilisées étaient Ionic et Angular5.",
+    link: { name: "Sportbak", url: "https://sportbak.com/" },
   },
 
   {
     title: "Hexacoffre (stage)",
     date: "Juillet 2019",
     description:
-      "Stage à temps plein réalisé chez Hexacoffre, entreprise de coffre-fort.\n\nLors de ce stage j'ai réalisé un site vitrine pour Hexacoffre spécialement conçu pour présenter ses services à la région Parisienne.\n\nLa technologie utilisée était Wordpress.",
+      "Stage à temps plein réalisé chez Hexacoffre, entreprise de coffre-fort.\n\nLors de ce stage j'ai réalisé un site vitrine pour Hexacoffre spécialement conçu pour présenter ses services à la région Parisienne.\n\nLa techno utilisée était Wordpress.",
+    link: { name: "Hexacoffre", url: "https://www.hexacoffre.com/fr/" },
   },
 ];
 
@@ -83,6 +90,16 @@ const Projects = () => {
               <Title position={position}>{project.title}</Title>
               <ProjectDate>{project.date}</ProjectDate>
               <Description>{project.description}</Description>
+              {project.link && (
+                <Link
+                  position={position}
+                  href={project.link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {project.link.name}
+                </Link>
+              )}
             </DescriptionWrapper>
           </ProjectWrapper>
         );
@@ -212,6 +229,18 @@ const ProjectDate = styled.p`
 const Description = styled.p`
   color: ${({ theme }) => theme.colors.textPrimary};
   white-space: pre-line;
+`;
+
+const Link = styled.a<{ position: "left" | "right" }>`
+  color: ${({ position, theme }) =>
+    position === "left" ? theme.colors.wave : theme.colors.violet};
+
+  filter: ${({ theme, position }) =>
+    position === "left"
+      ? `drop-shadow(0 0 8px ${theme.colors.wave})`
+      : `drop-shadow(0 0 8px ${theme.colors.violet})`};
+
+  text-decoration: none;
 `;
 
 export default Projects;
